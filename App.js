135da4a04525fd0b5de534a1903ddfcea93af036
Niwarthana <niwarthana.2016285@iit.ac.lk>
2019-09-09 10:47:10 +0530

    aws-otp-config

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import Amplify  from "aws-amplify";
-
 import { withAuthenticator, ChatBot } from "aws-amplify-react-native";
 import { Category } from "./source/Containers/Category.js";
 import awsmobile from "./source/constants/awsmobile";
